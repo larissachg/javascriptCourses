0b717c9528bf7820d04a7cf4eace6593a4226e49
Larissa <larissachg@gmail.com>
2023-09-04 17:43:18 -0400

    adding update appointment App with indexedDB

--- JSModernoJuanPablo/26-appointment-app/src/js/functions.js
+++ JSModernoJuanPablo/26-appointment-app/src/js/functions.js
@@ -14,7 +14,15 @@ import {
 const manageApointments = new Appointments();
 const ui = new UI(manageApointments);
 
-let editing;
+let editing = false;
+
+export let DB;
+
+export function database() {
+  window.onload = () => {
+    crearDB();
+  };
+}
 
 //Objeto Principal (Informacion de la Cita)
 const appointmentObj = {
@@ -52,25 +60,47 @@ export function newAppointment(e) {
   }
 
   if (editing) {
-    ui.showAlert("Se modifico correctamente");
-
     //Pasar el objeto de la cita a edicion
     manageApointments.editAppointment({ ...appointmentObj });
 
-    //Se vuelve el boton submit a su estado original
-    form.querySelector('button[type="submit"]').textContent = "Crear Cita";
+    //Edita en indexDB
+    const transaction = DB.transaction(["citas"], "readwrite");
+    const objectStore = transaction.objectStore("citas");
+    objectStore.put(appointmentObj);
 
-    //Quitar modo edicion
-    editing = false;
+    transaction.oncomplete = () => {
+      ui.showAlert("Se modifico correctamente");
+
+      //Se vuelve el boton submit a su estado original
+      form.querySelector('button[type="submit"]').textContent = "Crear Cita";
+
+      //Quitar modo edicion
+      editing = false;
+    };
+
+    transaction.onerror = () => {};
   } else {
+    //Nuevo Registro
+
     //Generar un ID unico
     appointmentObj.id = Date.now();
 
     //Crear una nueva cita
     manageApointments.addAppointment({ ...appointmentObj });
 
-    //Mensaje de agregado correctamente
-    ui.showAlert("Se agrego correctamente");
+    //Insertar registro en indexedDB
+    const transaction = DB.transaction(["citas"], "readwrite");
+
+    //Habilitar el objectStore
+    const objectStore = transaction.objectStore("citas");
+
+    //Insertar en la base de datos
+    objectStore.add(appointmentObj);
+
+    transaction.oncomplete = function () {
+      //Mensaje de agregado correctamente
+      ui.showAlert("Se agrego correctamente");
+    };
   }
 
   //Reiniciar el objeto para la validacion
@@ -80,7 +110,7 @@ export function newAppointment(e) {
   form.reset(); //funcion reset() ya viene por defecto para reiniciar
 
   //Mostrar el HTML con la cita creada
-  ui.showAppointments(manageApointments);
+  ui.showAppointments();
 }
 
 //Funcion para reiniciar el objeto de citas y que los datos vuelvan a espacios en blanco ''
@@ -95,14 +125,20 @@ export function resetObject() {
 
 //Funcion para eliminar una cita
 export function deleteAppointment(id) {
+  const transaction = DB.transaction(["citas"], "readwrite");
+  const objectStore = transaction.objectStore("citas");
+  objectStore.delete(id);
+
+  transaction.oncomplete = () => {
+    //Resfrescar las citas
+    ui.showAppointments();
+  };
+
   //Elminar la cita
   manageApointments.deleteAppointment(id);
 
   //Muestre un mensaje
   ui.showAlert("La cita se elimino correctamente");
-
-  //Resfrescar las citas
-  ui.showAppointments(manageApointments);
 }
 
 //Funcion para Cargar los datos y el modo edicion
@@ -129,4 +165,45 @@ export function uploadEdition(appointment) {
   //Cambiar el texto del boton
   form.querySelector('button[type="submit"]').textContent = "Guardar Cambios";
   editing = true;
+}
+
+function crearDB() {
+  //Crear la base de datos en version 1.0
+  const crearDB = window.indexedDB.open("citas", 1);
+
+  //Si hay un error
+  crearDB.onerror = function () {
+    console.log("Hubo un error");
+  };
+
+  //Si todo sale bien
+  crearDB.onsuccess = function () {
+    console.log("Base de datos creada");
+
+    DB = crearDB.result;
+
+    //Mostrar citas al cargar pero indexedBD ya esta listo
+    ui.showAppointments();
+  };
+
+  //Definamos las columnas
+  crearDB.onupgradeneeded = function (e) {
+    const db = e.target.result;
+
+    const objectStore = db.createObjectStore("citas", {
+      keyPath: "id",
+      autoIncrement: true,
+    });
+
+    //Definimos todas las columnas
+    objectStore.createIndex("pet", "pet", { unique: false });
+    objectStore.createIndex("owner", "owner", { unique: false });
+    objectStore.createIndex("phone", "phone", { unique: false });
+    objectStore.createIndex("date", "date", { unique: false });
+    objectStore.createIndex("time", "time", { unique: false });
+    objectStore.createIndex("symptoms", "symptoms", { unique: false });
+    objectStore.createIndex("id", "id", { unique: true });
+
+    console.log("DB creada y lista");
+  };
 }
